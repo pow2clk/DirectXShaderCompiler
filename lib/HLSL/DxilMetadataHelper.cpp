5a2b25a2a741946ef4eb80c7af64272b3637b5a8
Greg Roth <goyoroth@gmail.com>
2018-05-28 10:30:44 -0600

    [linux-port] Correct problem initializations (#214)
    Regardless of the order of variables placed in a constructor
    member initializer list, the variables are initialized in the
    order they are listed in the class or struct. This can cause
    issues where the assignment of one variable depends on the other.
    For this reason, a warning is produced if the member variables
    are listed in a different order. This change moves the member
    initializer lists into the order they were being initialized.
    Fixes 23 clang and 24 gcc warnings.
    
    Initialization of members of a larger aggregate type may benefit
    from braces to clarify which variables are expected to go where.
    At the same time, scalars should not be so segregated. This places
    or removes such braces wherever appropriate.
    Fixes 144 clang warnings
    
    Contributes to https://github.com/google/DirectXShaderCompiler/issues/206

--- lib/HLSL/DxilMetadataHelper.cpp
+++ lib/HLSL/DxilMetadataHelper.cpp
@@ -68,7 +68,7 @@ const char DxilMDHelper::kDxilSourceDefinesMDName[]                   = "dx.sour
 const char DxilMDHelper::kDxilSourceMainFileNameMDName[]              = "dx.source.mainFileName";
 const char DxilMDHelper::kDxilSourceArgsMDName[]                      = "dx.source.args";
 
-static std::array<const char *, 7> DxilMDNames = {
+static std::array<const char *, 7> DxilMDNames = { {
   DxilMDHelper::kDxilVersionMDName,
   DxilMDHelper::kDxilShaderModelMDName,
   DxilMDHelper::kDxilEntryPointsMDName,
@@ -76,11 +76,11 @@ static std::array<const char *, 7> DxilMDNames = {
   DxilMDHelper::kDxilTypeSystemMDName,
   DxilMDHelper::kDxilValidatorVersionMDName,
   DxilMDHelper::kDxilViewIdStateMDName,
-};
+}};
 
 DxilMDHelper::DxilMDHelper(Module *pModule, std::unique_ptr<ExtraPropertyHelper> EPH)
-: m_pModule(pModule)
-, m_Ctx(pModule->getContext())
+: m_Ctx(pModule->getContext())
+, m_pModule(pModule)
 , m_pSM(nullptr)
 , m_ExtraPropertyHelper(std::move(EPH)) {
 }
@@ -1336,8 +1336,8 @@ void DxilMDHelper::LoadDxilHSState(const MDOperand &MDO,
 // DxilExtraPropertyHelper methods.
 //
 DxilMDHelper::ExtraPropertyHelper::ExtraPropertyHelper(Module *pModule)
-: m_pModule(pModule)
-, m_Ctx(pModule->getContext()) {
+: m_Ctx(pModule->getContext())
+, m_pModule(pModule) {
 }
 
 DxilExtraPropertyHelper::DxilExtraPropertyHelper(Module *pModule)
