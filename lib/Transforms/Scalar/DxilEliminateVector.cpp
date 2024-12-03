24619ff610d81c04e77cc193889a171a7ccc54fe
Greg Roth <grroth@microsoft.com>
2024-12-02 19:14:45 -1000

    Validate vectors, allow in builtins
    introduces llvm instructions for needed vecotr manip operations so they are allowed in output
    
    skips vector-killing passes for 6.9 in a way that is robust for cases where you can't create a DXModule on the fly
    
    allow arg matching to let long vectors pass through in 6.9

--- lib/Transforms/Scalar/DxilEliminateVector.cpp
+++ lib/Transforms/Scalar/DxilEliminateVector.cpp
@@ -10,6 +10,8 @@
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
 
+#include "dxc/DXIL/DxilModule.h"
+
 #include "llvm/IR/Dominators.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Pass.h"
@@ -150,6 +152,11 @@ bool DxilEliminateVector::TryRewriteDebugInfoForVector(InsertElementInst *IE) {
 }
 
 bool DxilEliminateVector::runOnFunction(Function &F) {
+
+  const hlsl::ShaderModel *SM = F.getParent()->HasDxilModule()? F.getParent()->GetDxilModule().GetShaderModel(): nullptr;
+  if (SM && SM->IsSM69Plus()) {
+    return false;
+  }
 
   auto *DT = &getAnalysis<DominatorTreeWrapperPass>().getDomTree();
   DxilValueCache *DVC = &getAnalysis<DxilValueCache>();
