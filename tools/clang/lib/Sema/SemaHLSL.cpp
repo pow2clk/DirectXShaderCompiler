2287709ffc37147838eb9a45afdcb254def52908
Greg Roth <grroth@microsoft.com>
2024-12-05 10:55:40 -1000

    Add error for long vectors in cbuffer
    the 16 byte limit there prevents this from being possible and I'm not interested in touching that

--- tools/clang/lib/Sema/SemaHLSL.cpp
+++ tools/clang/lib/Sema/SemaHLSL.cpp
@@ -14214,6 +14214,7 @@ bool Sema::DiagnoseHLSLDecl(Declarator &D, DeclContext *DC, Expr *BitWidth,
                        *pDispatchGrid = nullptr, *pMaxDispatchGrid = nullptr;
   bool usageIn = false;
   bool usageOut = false;
+  bool isGroupShared = false;
 
   for (clang::AttributeList *pAttr = D.getDeclSpec().getAttributes().getList();
        pAttr != NULL; pAttr = pAttr->getNext()) {
@@ -14237,6 +14238,7 @@ bool Sema::DiagnoseHLSLDecl(Declarator &D, DeclContext *DC, Expr *BitWidth,
       }
       break;
     case AttributeList::AT_HLSLGroupShared:
+      isGroupShared = true;
       if (!isGlobal) {
         Diag(pAttr->getLoc(), diag::err_hlsl_varmodifierna)
             << pAttr->getName() << declarationType << pAttr->getRange();
@@ -14514,6 +14516,13 @@ bool Sema::DiagnoseHLSLDecl(Declarator &D, DeclContext *DC, Expr *BitWidth,
        basicKind == ArBasicKind::AR_OBJECT_POINTSTREAM ||
        basicKind == ArBasicKind::AR_OBJECT_TRIANGLESTREAM)) {
     Diag(D.getLocStart(), diag::err_hlsl_missing_inout_attr);
+    result = false;
+  }
+
+  // Vectors of over 4 elements can't fit in the cbuffer 16 byte registers.
+  if (hlsl::IsVectorType(this, qt) && hlsl::GetElementCount(qt) > 4 &&
+      isGlobal && !isStatic && !isGroupShared) {
+    Diag(D.getLocStart(), diag::err_hlsl_long_vector_in_cbuffer);
     result = false;
   }
 
---
+++ tools/clang/test/SemaHLSL/longvecs.hlsl
@@ -0,0 +1,9 @@
+// RUN: %dxc -Tlib_6_9 -verify %s
+
+vector<float, 8> vec1; // expected-error {{Vectors of over 4 elements in cbuffers are not supported}}
+cbuffer CBUF {
+  vector<float, 8> vec2; // expected-error {{Vectors of over 4 elements in cbuffers are not supported}}
+};
+
+groupshared vector<float, 8> vec34;
+static vector<float, 8> vec4;
