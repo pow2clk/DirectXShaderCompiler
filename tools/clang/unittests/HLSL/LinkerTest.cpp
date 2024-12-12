6f6d9e946accd631b24cf25c387935bef9684dce
Greg Roth <grroth@microsoft.com>
2024-12-12 16:10:37 -0700

    adapt behavior of matrix bitcast lower for vectors
    This now adapts to the linker target to either preserve vectors for 6.9 or arrays for previous versions.
    
    This requires running the DynamicIndexing VectorToArray pass during linking since 6_x and 6_9+ will fail to run this in the initial compile, but will still need to lower vectors to arrays.

--- tools/clang/unittests/HLSL/LinkerTest.cpp
+++ tools/clang/unittests/HLSL/LinkerTest.cpp
@@ -523,9 +523,13 @@ TEST_F(LinkerTest, RunLinkMatArrayParam) {
   LPCWSTR libName2 = L"test";
   RegisterDxcModule(libName2, pLib, pLinker);
 
-  Link(L"main", L"ps_6_9", pLinker, {libName, libName2},
+  Link(L"main", L"ps_6_0", pLinker, {libName, libName2},
        {"alloca [24 x float]", "getelementptr [12 x float], [12 x float]*"},
-       {});// fails validation somehow
+       {});
+
+  Link(L"main", L"ps_6_9", pLinker, {libName, libName2},
+       {"alloca [2 x <12 x float>]", "getelementptr [12 x float], [12 x float]*"},
+       {});
 }
 
 TEST_F(LinkerTest, RunLinkMatParam) {
