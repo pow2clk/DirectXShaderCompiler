b286d2ff61ad0ab5f47c53b61511aa7c907c622e
Greg Roth <grroth@microsoft.com>
2024-12-13 11:43:04 -0700

    update test for groupshared and vec sizes

--- tools/clang/test/SemaHLSL/longvecs.hlsl
+++ tools/clang/test/SemaHLSL/longvecs.hlsl
@@ -1,5 +1,7 @@
 // RUN: %dxc -Tlib_6_9 -verify %s
 
+// TODO add signature testing.
+
 vector<float, 8> vec1; // expected-error {{Vectors of over 4 elements in cbuffers are not supported}}
 cbuffer CBUF {
   vector<float, 8> vec2; // expected-error {{Vectors of over 4 elements in cbuffers are not supported}}
